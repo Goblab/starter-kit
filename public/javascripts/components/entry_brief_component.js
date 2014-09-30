167045dd99a32bf3897148df53136261614df85d
Maximiliano Ivan Biondo <mbiondo@goblab.org>
2014-09-30 02:02:43 -0300

    Entry animation fadeIn

--- public/javascripts/components/entry_brief_component.js
+++ public/javascripts/components/entry_brief_component.js
@@ -1,11 +1,16 @@
 var EntryBriefComponent = Ember.Component.extend({
-	classNames:['entry-brief'],
+	classNames:['entry-brief', 'fade-out'],
 	format: 'LL',
 	formattedDate: function() {
 		var date = this.get('model.createdAt'),
 		    format = this.get('format');
 		return moment(date).calendar();
-	}.property('model.createdAt', 'format')
+	}.property('model.createdAt', 'format'),
+
+	initialize: function () {
+		this.$().fadeIn(500);
+	}.on('didInsertElement'),
+
 });
 
 module.exports = EntryBriefComponent;
