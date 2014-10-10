78babc86e21a1bdd8ddf84633bd35ec65f1ce63b
mbiondo <mbiondo@goblab.org>
2014-10-10 11:27:55 -0300

    Delete person_form_wrapper.js

--- public/javascripts/helpers/person_form_wrapper.js
+++
@@ -1,7 +0,0 @@
-Ember.EasyForm.Config.registerWrapper('personFormWrapper', {
-  formClass: 'form-horizontal',
-  errorClass: '',
-  hintClass: 'help-block',
-  labelClass: '',
-  inputClass: 'form-control',
-});
