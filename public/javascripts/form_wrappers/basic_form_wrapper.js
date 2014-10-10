5f086e75c88db758dfc17e44ae8a174e5cf4281f
santuan <santuan@santuan-VirtualBox.(none)>
2014-10-10 13:47:32 -0300

    Off canvas

---
+++ public/javascripts/form_wrappers/basic_form_wrapper.js
@@ -0,0 +1,17 @@
+Ember.EasyForm.Config.registerWrapper('basicFormWrapper', {
+  // Define the custom template
+  inputTemplate: 'wrappers/basic',
+
+  // Define a custom config used by the template
+  controlsWrapperClass: 'basic-controls',
+
+  // Define the classes for the form, label, error...
+  formClass: 'basic-form',
+  fieldErrorClass: 'basic-error',
+  errorClass: 'basic-help-inline',
+  hintClass: 'basic-help-block',
+  labelClass: 'basic-control-label',
+  inputClass: 'basic-control-group'  
+});
+
+
