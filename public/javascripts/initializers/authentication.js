f19ca6212badfd742a229b6b882a766b637d445b
Maximiliano Biondo <mbiondo@goblab.org>
2014-11-10 14:15:15 -0300

    Session bug fixed

--- public/javascripts/initializers/authentication.js
+++ public/javascripts/initializers/authentication.js
@@ -8,15 +8,14 @@ var Authentication = Ember.Application.initializer({
         var userId = this.get('user_id');
         var _self = this;
         if (!Ember.isEmpty(userId)) {
-          return new Ember.RSVP.Promise(function(resolve, reject) {
-            container.lookup('store:main').find('user', userId).then(function (user) {
-              resolve(user);
-            }, function (error) {
-              _self.invalidate();
-              reject();
-            });            
-          });
+          container.lookup('store:main').find('user', userId).then(function (user) {
+
+          }, function (error) {
+            _self.invalidate();
+          });            
+          return container.lookup('store:main').find('user', userId);
         }
+
       }.property('user_id'),
     });
     Ember.SimpleAuth.setup(container, application, {
--- public/javascripts/routes/index_route.js
+++ public/javascripts/routes/index_route.js
@@ -1,7 +1,4 @@
 var IndexRoute = Ember.Route.extend({
-  model: function  () {
-	return this.get('store').find('entry');
-  }
 });
 
 module.exports = IndexRoute;
